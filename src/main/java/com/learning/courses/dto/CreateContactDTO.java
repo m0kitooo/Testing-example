fdfbc355c7e3112556880435495151a3fbf7a2f4
m0kitooo <mikolajklimuszko123@gmail.com>
2026-05-20 22:50:39 +0200

    fix: validation added to CreateContactDTO

--- src/main/java/com/learning/courses/dto/CreateContactDTO.java
+++ src/main/java/com/learning/courses/dto/CreateContactDTO.java
@@ -1,6 +1,10 @@
 package com.learning.courses.dto;
 
 import jakarta.validation.Valid;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -14,7 +18,13 @@ import java.util.List;
 @SuperBuilder
 @Valid
 public class CreateContactDTO {
-    List<String> emails;
-    List<String> phoneNumbers;
-    List<String> addresses;
+
+    @NotEmpty(message = "At least one email is required, passing null allowed")
+    private List<@NotNull(message = "Email cannot be null") @Email(message = "Invalid email format") String> emails;
+
+    @NotEmpty(message = "At least one phone number is required, passing null allowed")
+    private List<@NotBlank(message = "Phone number cannot be blank") String> phoneNumbers;
+
+    @NotEmpty(message = "At least one address is required, passing null allowed")
+    private List<@NotBlank(message = "Address cannot be blank") String> addresses;
 }
