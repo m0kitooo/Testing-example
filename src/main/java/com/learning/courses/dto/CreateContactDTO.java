26053d1576577ebda0380b90387c7baf77baf3b6
m0kitooo <mikolajklimuszko123@gmail.com>
2026-05-21 01:21:38 +0200

    fix: logic error in dto validation fixed

--- src/main/java/com/learning/courses/dto/CreateContactDTO.java
+++ src/main/java/com/learning/courses/dto/CreateContactDTO.java
@@ -19,12 +19,9 @@ import java.util.List;
 @Valid
 public class CreateContactDTO {
 
-    @NotEmpty(message = "At least one email is required, passing null allowed")
     private List<@NotNull(message = "Email cannot be null") @Email(message = "Invalid email format") String> emails;
 
-    @NotEmpty(message = "At least one phone number is required, passing null allowed")
     private List<@NotBlank(message = "Phone number cannot be blank") String> phoneNumbers;
 
-    @NotEmpty(message = "At least one address is required, passing null allowed")
     private List<@NotBlank(message = "Address cannot be blank") String> addresses;
 }
