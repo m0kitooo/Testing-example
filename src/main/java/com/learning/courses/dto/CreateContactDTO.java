c1ff40e8c1e51b4ebd275ee522683f0d1fb1600f
m0kitooo <mikolajklimuszko123@gmail.com>
2026-05-21 08:12:41 +0200

    fix: changed phone number validation logic, now using regex for validation

--- src/main/java/com/learning/courses/dto/CreateContactDTO.java
+++ src/main/java/com/learning/courses/dto/CreateContactDTO.java
@@ -1,10 +1,7 @@
 package com.learning.courses.dto;
 
 import jakarta.validation.Valid;
-import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotEmpty;
-import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -21,7 +18,10 @@ public class CreateContactDTO {
 
     private List<@NotNull(message = "Email cannot be null") @Email(message = "Invalid email format") String> emails;
 
-    private List<@NotBlank(message = "Phone number cannot be blank") String> phoneNumbers;
+    private List<
+            @NotBlank(message = "Phone number cannot be null")
+            @Pattern(regexp = "^(\\+48|48)?[\\s-]?(\\d{3}[\\s-]?){2}\\d{3}$", message = "Invalid Polish phone number format") String>
+            phoneNumbers;
 
     private List<@NotBlank(message = "Address cannot be blank") String> addresses;
 }
